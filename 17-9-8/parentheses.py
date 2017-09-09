87e3c9ac7efee5ca130c11a45ecd65a1969fdea4
cyw <weems1995@hotmail.com>
2017-09-09 10:03:36 +0800

    Revert "9.8"
    This reverts commit 1a053bc19e3a002a43cba1c9e16f387da06e0263.

--- 17-9-8/parentheses.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def generateParenthesis(self, n):
-        def generator(p,left,right):
-            if right>=left>=0:
-                if not right:
-                    yield p
-                for q in generator(p+'(',left+1,right): yield q
-                for q in generator(p+')',left,right-1): yield q
-        return list(generator('',n,n))
