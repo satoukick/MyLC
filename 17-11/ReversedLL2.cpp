5735e6ed900084965e06174d465f11b7d6257e89
cyw <weems1995@hotmail.com>
2017-11-29 21:14:19 +0800

    11.29

---
+++ 17-11/ReversedLL2.cpp
@@ -0,0 +1,14 @@
+// Definition for singly-linked list.
+struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+	ListNode* reverseBetween(ListNode* head, int m, int n) {
+		ListNode *new_head = new ListNode(0);
+
+	}
+};
