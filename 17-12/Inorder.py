deabccf7a4eccb77d1e3eff681e7b7354d7b1820
cyw <weems1995@hotmail.com>
2017-12-05 23:28:52 +0800

    incursive inorder

--- 17-12/Inorder.py
+++ 17-12/Inorder.py
@@ -6,7 +6,7 @@
 #         self.right = None
 
 class Solution:
-    def inorderTraversal(self, root):
+    def inorderTraversal(self, root):  # 66ms
         """
         :type root: TreeNode
         :rtype: List[int]
@@ -20,3 +20,16 @@ class Solution:
             self.helper(node.left,res)
             res.append(node.val)
             self.helper(node.right,res)
+
+    def iterative_inorder_traversal(self,root):  # 59ms
+        res, stack = [], []
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            if not stack:
+                return res
+            node = stack.pop()
+            res.append(node.val)
+            root = node.right
+
