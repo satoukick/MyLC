30a043076e88ce1dd53511123e7052475c4af27d
cyw <weems1995@hotmail.com>
2018-03-19 21:47:40 +0800

    3.19

---
+++ 18-03/PopulateNextRightPointers2/Solution.java
@@ -0,0 +1,31 @@
+package PopulateNextRightPointers2;
+
+
+class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right, next;
+    TreeLinkNode(int x) { val = x; }
+}
+
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode tempChild = new TreeLinkNode(0);
+        while (root != null) {
+            TreeLinkNode currentChild = tempChild;
+            while(root != null) {
+                if (root.left != null) {
+                    currentChild.next = root.left;
+                    currentChild = currentChild.next;
+                }
+                if (root.right != null) {
+                    currentChild.next = root.right;
+                    currentChild = currentChild.next;
+                }
+                root = root.next;
+            }
+            root = tempChild.next;
+            tempChild.next = null;
+        }
+
+    }
+}
